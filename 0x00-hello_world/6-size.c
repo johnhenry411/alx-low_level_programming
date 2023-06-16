cbbb5548634844dd1ff0b450ad99e04e374b0b3f
johnhenry411 <henryowino517@gmail.com>
2023-06-16 02:40:07 -0700

    eey

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,10 @@
 #include<stdio.h>
-int main()
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
@@ -8,4 +13,3 @@ int main()
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
