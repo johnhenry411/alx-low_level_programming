6624063103cce3e3e21aa5aeab1dda77e92f7887
johnhenry411 <henryowino517@gmail.com>
2023-06-20 14:07:22 -0700

    goodnight

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,39 @@
+/*
+ * File: 101-natural.c
+ * Auth: vincent kip
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Lists all the natural numbers below 1024 (excluded)
+ *        that are multiples of 3 or 5.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
+
+102-fibonacci.c
+
+#include <stdio.h>
+
+/**
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
