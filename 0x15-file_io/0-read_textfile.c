944805751d69a8dd6506980311ab34e004f14c2a
johnhenry411 <henryowino517@gmail.com>
2023-08-07 13:34:58 -0700

    files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,45 +1,29 @@
-#include "main.h" 
+#include "main.h"
 #include <stdlib.h>
-#include <fcntl.h>  
-#include <unistd.h> 
 
 /**
- * read_textfile - Read text file and print to STDOUT.
- * @filename: Text file to be read.
- * @letters: Number of letters to read.
- * Return: The number of bytes read and printed, or 0 on failure.
+ * read_textfile- Read text file print to STDOUT.
+ * @filename: text file being read
+ * @letters: number of letters to be read
+ * Return: w- actual number of bytes read and printed
+ *        0 when function fails or filename is NULL.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    char *buf;        
-    ssize_t fd;       
-    ssize_t bytes_read; 
-    ssize_t bytes_written; 
-    fd = open(filename, O_RDONLY);
-    if (fd == -1) 
-        return (0);
-    buf = malloc(sizeof(char) * letters);
-    if (buf == NULL) 
-    {
-        close(fd);
-return (0);
-    }
-    bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1) 
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-    bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    if (bytes_written == -1) 
-    {
-        free(buf);
-        close(fd);
-        return (0);
-    }
-    free(buf);
-    close(fd);
-    return (bytes_written);
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
 
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,7 +20,7 @@ char *create_buffer(char *file)
 	if (buffer == NULL)
 	{
 		dprintf(STDERR_FILENO,
-			"Error: Cannot commit to %s\n", file);
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
@@ -39,7 +39,7 @@ void close_file(int fd)
 
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Cannot terminate fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
 		exit(100);
 	}
 }
@@ -76,7 +76,7 @@ int main(int argc, char *argv[])
 		if (from == -1 || r == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't execute from file %s\n", argv[1]);
+				"Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -85,7 +85,7 @@ int main(int argc, char *argv[])
 		if (to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't decode to %s\n", argv[2]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
@@ -101,4 +101,5 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
+
 
